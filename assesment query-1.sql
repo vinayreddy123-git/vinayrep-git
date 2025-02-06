8035457cf0597fd1573bd6605facf4ec4b7a4273
vinayreddy123-git <vinayreddy.vkr22@gmail.com>
2025-02-06 11:25:32 +0530

    Add files via upload

---
+++ assesment query-1.sql
@@ -0,0 +1,46 @@
+SELECT SUM(TransactionAmount) AS TotalRevenue  
+FROM assesment
+
+SELECT CustomerID, AVG(TransactionAmount) AS AvgTransactionAmount  
+FROM assesment  
+GROUP BY CustomerID;
+
+
+SELECT City, SUM(TransactionAmount) AS TotalRevenue  
+FROM assesment  
+GROUP BY City  
+ORDER BY TotalRevenue DESC 
+
+
+SELECT PaymentMethod, COUNT(*) AS UsageCount  
+FROM assesment  
+GROUP BY PaymentMethod  
+ORDER BY UsageCount DESC
+
+SELECT ProductName, SUM(Quantity) AS TotalQuantitySold  
+FROM assesment  
+GROUP BY ProductName  
+ORDER BY TotalQuantitySold DESC
+
+
+SELECT  
+    (COUNT(CASE WHEN DiscountPercent > 0 THEN 1 END) * 100.0 / COUNT(*)) AS DiscountedTransactionPercentage  
+FROM assesment
+
+ 
+SELECT ProductName, COUNT(*) AS ReturnCount  
+FROM assesment  
+WHERE Returned = 'Yes'  
+GROUP BY ProductName  
+ORDER BY ReturnCount DESC
+
+
+SELECT Region, AVG(DeliveryTimeDays) AS AvgDeliveryTime  
+FROM assesment  
+GROUP BY Region
+
+SELECT StoreType, AVG(FeedbackScore) AS AvgFeedbackScore  
+FROM assesment 
+GROUP BY StoreType  
+ORDER BY AvgFeedbackScore DESC
+
