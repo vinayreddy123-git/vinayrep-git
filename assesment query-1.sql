bf419199469d15c604d4475a3f02811eed476eff
vinayreddy123-git <vinayreddy.vkr22@gmail.com>
2025-02-06 11:42:26 +0530

    Add files via upload
    Assesment queries

--- assesment query-1.sql
+++ assesment query-1.sql
@@ -1,21 +1,30 @@
+//Total Sales Revenue
+
 SELECT SUM(TransactionAmount) AS TotalRevenue  
 FROM assesment
 
+
+//Average Transaction Amount per Customer
+
 SELECT CustomerID, AVG(TransactionAmount) AS AvgTransactionAmount  
 FROM assesment  
-GROUP BY CustomerID;
+GROUP BY CustomerID
 
+//Top 5 Cities by Sales Revenue
 
 SELECT City, SUM(TransactionAmount) AS TotalRevenue  
 FROM assesment  
 GROUP BY City  
 ORDER BY TotalRevenue DESC 
 
+//Most Popular Payment Method
 
 SELECT PaymentMethod, COUNT(*) AS UsageCount  
 FROM assesment  
 GROUP BY PaymentMethod  
 ORDER BY UsageCount DESC
+
+//Highest Selling Product by Quantity
 
 SELECT ProductName, SUM(Quantity) AS TotalQuantitySold  
 FROM assesment  
@@ -23,10 +32,15 @@ GROUP BY ProductName
 ORDER BY TotalQuantitySold DESC
 
 
+
+//Percentage of Transactions with Discounts
+
 SELECT  
     (COUNT(CASE WHEN DiscountPercent > 0 THEN 1 END) * 100.0 / COUNT(*)) AS DiscountedTransactionPercentage  
 FROM assesment
 
+
+//Number of Returns Per Product
  
 SELECT ProductName, COUNT(*) AS ReturnCount  
 FROM assesment  
@@ -35,9 +49,13 @@ GROUP BY ProductName
 ORDER BY ReturnCount DESC
 
 
+//Average Delivery Time by Region
+
 SELECT Region, AVG(DeliveryTimeDays) AS AvgDeliveryTime  
 FROM assesment  
 GROUP BY Region
+
+//Customer Satisfaction Analysis
 
 SELECT StoreType, AVG(FeedbackScore) AS AvgFeedbackScore  
 FROM assesment 
